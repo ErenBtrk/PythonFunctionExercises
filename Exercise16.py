a27003cb39743cfa735f7124829539b114e150c4
= <=>
2021-05-25 02:30:47 +0300

    Exercise 16

---
+++ Exercise16.py
@@ -0,0 +1,10 @@
+'''
+16. Write a Python function to create and print a list where the values
+ are square of numbers between 1 and 30 (both included).
+ 
+'''
+
+def function():
+    return [item**2 for item in range(1,31)]
+
+print(function())
